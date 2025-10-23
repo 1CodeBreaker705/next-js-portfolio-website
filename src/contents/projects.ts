db279be4f1c8df9971aaa252737fe8099c49de2c
1CodeBreaker705 <ranjansingh0661@gmail.com>
2025-10-23 22:35:42 +0530

    Update projects.ts

--- src/contents/projects.ts
+++ src/contents/projects.ts
@@ -20,8 +20,8 @@ export const projects:Project[] = [
         title: 'Portfolio Website',
         description: 'My personal portfolio website showcasing my projects and skills.',
         technologies: ['Next.js', 'Tailwind CSS', 'Framer Motion'],
-        githubLink: 'https://github.com',
-        demoLink: 'https://demo.com',
-        image: '/projects/portfolio-website.jpg',
+        githubLink: 'https://github.com/1CodeBreaker705/next-js-portfolio-website',
+        demoLink: 'https://next-js-portfolio-website-livid.vercel.app/',
+        image: '/projects/My-Portfolio-Website.png',
       },
   ];
