65c03d06a04740cebe77a37f056131658990c4dd
1CodeBreaker705 <ranjansingh0661@gmail.com>
2026-05-24 03:34:36 +0530

    Update projects.ts

--- src/contents/projects.ts
+++ src/contents/projects.ts
@@ -1,6 +1,14 @@
 import { Project } from "@/types";
 export const projects:Project[] = [
       {
+        title: 'Portfolio Website',
+        description: 'My personal portfolio website showcasing my projects and skills.',
+        technologies: ['Next.js', 'Tailwind CSS', 'Framer Motion'],
+        githubLink: 'https://github.com/1CodeBreaker705/next-js-portfolio-website',
+        demoLink: 'https://next-js-portfolio-website-livid.vercel.app/',
+        image: '/projects/My-Portfolio-Website.png',
+      },
+      {
         title: 'Taskin',
         description: 'A full-stack task management app that lets users securely create, organize, and track their tasks with JWT authentication and a modern React interface.',
         technologies: ['React', 'Node.js','Express', 'MongoDB'],
@@ -17,11 +25,11 @@ export const projects:Project[] = [
         image: '/projects/Blog-Website.png',
       },
       {
-        title: 'Portfolio Website',
-        description: 'My personal portfolio website showcasing my projects and skills.',
-        technologies: ['Next.js', 'Tailwind CSS', 'Framer Motion'],
-        githubLink: 'https://github.com/1CodeBreaker705/next-js-portfolio-website',
-        demoLink: 'https://next-js-portfolio-website-livid.vercel.app/',
-        image: '/projects/My-Portfolio-Website.png',
+        title: 'Snapbuy',
+        description: 'A production-style multi-vendor ecommerce platform that enables customers and merchants to manage products, orders, payments, and shopping workflows securely.',
+        technologies: ['React','Tailwind CSS', 'FastAPI', 'MongoDB', 'Razorpay', 'Cloudinary'],
+        githubLink: 'https://github.com/1CodeBreaker705/SnapBuy-Ecommerce-Website',
+        demoLink: 'https://snap-buy-ecommerce-website.vercel.app/',
+        image: '/projects/SnapBuy-Ecommerce-Website.png',
       }
   ];
