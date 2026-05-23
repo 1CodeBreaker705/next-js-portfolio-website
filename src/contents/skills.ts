45ac5e107bc827b2efbd7c70f866764cf2f70110
1CodeBreaker705 <ranjansingh0661@gmail.com>
2026-05-24 04:13:01 +0530

    Update skills.ts

--- src/contents/skills.ts
+++ src/contents/skills.ts
@@ -1,6 +1,22 @@
 import { SkillInfo } from "@/types";
 export const skills: SkillInfo[] = [
   {
+    title: "C",
+    image: "https://www.svgrepo.com/show/373482/c.svg",
+  },
+  {
+    title: "C++",
+    image: "https://www.svgrepo.com/show/376358/c-plus-plus.svg",
+  },
+  {
+    title: "Python",
+    image: "https://www.svgrepo.com/show/452091/python.svg",
+  },
+  {
+    title: "Java",
+    image: 'https://www.svgrepo.com/show/452234/java.svg'
+  },
+  {
     title: "Html",
     image: "https://www.svgrepo.com/show/452228/html-5.svg",
   },
@@ -37,6 +53,10 @@ export const skills: SkillInfo[] = [
     image: "https://www.svgrepo.com/show/330398/express.svg",
   },
   {
+    title: "FastAPI",
+    image: "https://i.postimg.cc/kgxdtdw0/fastapi-svgrepo-com.jpg",
+  },
+  {
     title: "PostgreSQL",
     image: "https://www.svgrepo.com/show/373965/pgsql.svg",
   },
@@ -45,7 +65,20 @@ export const skills: SkillInfo[] = [
     image: "https://www.svgrepo.com/show/373845/mongo.svg",
   },
   {
-    title: "Java",
-    image: 'https://www.svgrepo.com/show/452234/java.svg'
-  }
+    title: "Redux-Redux Toolkit",
+    image: "https://www.svgrepo.com/show/452093/redux.svg",
+  },
+  {
+    title: "Zustand",
+    image: "https://user-images.githubusercontent.com/958486/218346783-72be5ae3-b953-4dd7-b239-788a882fdad6.svg",
+  },
+   {
+    title: "Git",
+    image: "https://www.svgrepo.com/show/452210/git.svg",
+  },
+  {
+    title: "Docker",
+    image: "https://www.svgrepo.com/show/448221/docker.svg",
+  },
+  
 ];
