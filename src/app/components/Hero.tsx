98973b5fcb6c8786c43354b9181fa96048a2962f
1CodeBreaker705 <ranjansingh0661@gmail.com>
2026-05-25 22:34:26 +0530

    Update Hero.tsx

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -77,13 +77,13 @@ const Hero = () => {
           <div className="flex flex-col md:flex-row justify-center gap-4 w-full md:w-auto">
             <Link
               href="/projects"
-              className="bg-primary inline-block w-full md:w-auto text-white px-5 py-2 rounded-lg hover:bg-primary/75 transition-colors"
+              className="bg-primary inline-block w-1/2 md:w-auto text-white px-5 py-2 rounded-lg hover:bg-primary/75 transition-colors"
             >
               View Projects
             </Link>
             <Link
               href="/contact"
-              className="border border-gray-400 text-gray-700 inline-block w-full md:w-auto px-5 py-2 rounded-lg hover:bg-gray-100 dark:text-gray-400 dark:hover:text-gray-700 transition-colors"
+              className="border border-gray-400 text-gray-700 inline-block w-1/2 md:w-auto px-5 py-2 rounded-lg hover:bg-gray-100 dark:text-gray-400 dark:hover:text-gray-700 transition-colors"
             >
               Contact Me
             </Link>
