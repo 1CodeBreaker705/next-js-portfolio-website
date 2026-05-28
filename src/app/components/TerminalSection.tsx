eb966a6021ff0f0337edd08b2bca103056b7e415
1CodeBreaker705 <ranjansingh0661@gmail.com>
2026-05-28 16:07:15 +0530

    Update TerminalSection.tsx

--- src/app/components/TerminalSection.tsx
+++ src/app/components/TerminalSection.tsx
@@ -49,7 +49,7 @@ const TerminalSection = () => {
       if (index > fullText.length) {
         clearInterval(interval);
       }
-    }, 32);
+    }, 35);
 
     return () => clearInterval(interval);
   }, [isInView]);
