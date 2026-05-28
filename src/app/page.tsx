aa1ed7eb326952bddd2da51aa2ed58f31d45e2b4
1CodeBreaker705 <ranjansingh0661@gmail.com>
2026-05-28 13:17:51 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Hero from "./components/Hero";
-import Newsletter from "./components/Newsletter";
+import TerminalSection from "./components/TerminalSection";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
@@ -9,7 +9,7 @@ export default function Home() {
      <Hero/>
      <Projects/>
      <Skills/>
-     <Newsletter/>
+     <TerminalSection/>
     </>
   );
 }
