9caf6fc289d7e18f0b2b8a633edfc127f61e00b0
1CodeBreaker705 <ranjansingh0661@gmail.com>
2026-05-28 13:23:36 +0530

    Update TerminalSection.tsx

--- src/app/components/TerminalSection.tsx
+++ src/app/components/TerminalSection.tsx
@@ -5,24 +5,25 @@ import { Typewriter } from "react-simple-typewriter";
 
 const TerminalSection = () => {
   return (
-    <section className="mt-20 mb-20">
-      <div className="container max-w-5xl mx-auto px-4">
+    <section className="pb-16">
+      <div className="container max-w-7xl mx-auto px-4">
 
-        <div className="bg-[#0d1117] border border-gray-800 rounded-2xl shadow-2xl overflow-hidden">
+        <div className="w-full bg-white dark:bg-[#0d1117] border border-gray-300 dark:border-gray-800 rounded-2xl shadow-2xl overflow-hidden transition-colors duration-300">
 
           {/* terminal header */}
-          <div className="flex items-center gap-2 px-4 py-3 bg-[#161b22] border-b border-gray-800">
+          <div className="flex items-center gap-2 px-4 py-3 bg-gray-100 dark:bg-[#161b22] border-b border-gray-300 dark:border-gray-800 transition-colors duration-300">
+
             <div className="w-3 h-3 rounded-full bg-red-500"></div>
             <div className="w-3 h-3 rounded-full bg-yellow-500"></div>
             <div className="w-3 h-3 rounded-full bg-green-500"></div>
 
-            <span className="ml-4 text-sm text-gray-400 font-mono">
+            <span className="ml-4 text-sm text-gray-500 dark:text-gray-400 font-mono">
               ranjan@portfolio:~
             </span>
           </div>
 
           {/* terminal body */}
-          <div className="p-6 md:p-8 font-mono text-sm md:text-base text-gray-300 leading-relaxed">
+          <div className="p-6 md:p-8 font-mono text-sm md:text-base text-black dark:text-gray-300 leading-relaxed whitespace-pre-line transition-colors duration-300">
 
             <Typewriter
               words={[
